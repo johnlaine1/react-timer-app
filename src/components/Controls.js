0afc82514a8157f2f7e26b3808160bf79680c246
John Laine <john.laine5896@gmail.com>
2017-03-06 23:43:15 +0000

    Issue #3 Make the button look a little better
    Closes #3

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -10,17 +10,17 @@ class Controls extends Component {
     const {countdownStatus} = this.props;
     
     if (countdownStatus === 'started') {
-      return <button onClick={this.onStatusChange('paused')} >Pause</button>;
+      return <button className="btn btn-info"onClick={this.onStatusChange('paused')} >Pause</button>;
     }
     if (countdownStatus === 'paused' || countdownStatus === 'stopped') {
-      return <button onClick={this.onStatusChange('started')}>Start</button>;
+      return <button className="btn btn-primary"onClick={this.onStatusChange('started')}>Start</button>;
     }
   }
   render() {
     return (
       <div>
         {this.renderStartStopButton()}
-        <button onClick={this.onStatusChange('stopped')}>Clear</button>
+        <button className="btn btn-default"onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     ); 
   }
